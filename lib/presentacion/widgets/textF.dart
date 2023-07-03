ba5b5a69048c88febdf642c05775ccf685f5c019
Delta27222 <angelgabriel.hernandez@gmail.com>
2023-07-03 13:21:13 -0700

    Feature - Agregando botones de funciones premium:

--- lib/presentacion/widgets/textF.dart
+++ lib/presentacion/widgets/textF.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: use_key_in_widget_constructors, library_private_types_in_public_api, file_names
 
 import 'package:flutter/material.dart';
+import 'package:notea_frontend/presentacion/pantallas/Speech_to_Text_Screen.dart';
 import 'package:rich_editor/rich_editor.dart';
 
 class TextBlockPrueba1 extends StatefulWidget {
@@ -21,13 +22,29 @@ class _TextBlockPrueba1State extends State<TextBlockPrueba1> {
     super.dispose();
   }
 
-Future<String?>? getEditorValue()async{
-  if (widget._editorKey.currentState != null) {
-    String? html = await widget._editorKey.currentState?.getHtml();
-    return html;
+  Future<String?>? getEditorValue() async {
+    if (widget._editorKey.currentState != null) {
+      String? html = await widget._editorKey.currentState?.getHtml();
+      return html;
+    }
+    return null;
   }
-  return null;
-}
+
+  void customAction(String action) {
+    // Lógica para las acciones personalizadas
+    if (action == 'action1') {
+      Navigator.push(context, 
+        MaterialPageRoute(builder: (context) => 
+          const SpeechToTextScreen(textoNota: '')))
+          .then((value) => setState(() {
+            //nos devuelve el texto de la nota
+            }));
+      // Acción 1
+    } else if (action == 'action2') {
+      print('Accion 2');
+      // Acción 2
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,20 +59,36 @@ Future<String?>? getEditorValue()async{
             width: 1.0,
           ),
         ),
-        child:
-              RichEditor(
+        child: Column(
+          children: [
+            Expanded(
+              child: RichEditor(
                 key: widget._editorKey,
-                //Aca agregamos lo que nos trae el contenido de la nota
-                value: '''
-                  
-                ''',
+                value: '',
                 editorOptions: RichEditorOptions(
                   placeholder: 'Start typing',
                   padding: const EdgeInsets.symmetric(horizontal: 20.0),
                   baseFontFamily: 'sans-serif',
                   barPosition: BarPosition.BOTTOM,
                 ),
-              )
+              ),
+            ),
+            PopupMenuButton<String>(
+              icon: const Icon(Icons.blur_on),
+              itemBuilder: (context) => [
+                const PopupMenuItem<String>(
+                  value: 'action1',
+                  child: Text('Audio a Texto'),
+                ),
+                const PopupMenuItem<String>(
+                  value: 'action2',
+                  child: Text('Imagen a Texto'),
+                ),
+              ],
+              onSelected: customAction,
+            ),
+          ],
+        ),
       ),
     );
   }
